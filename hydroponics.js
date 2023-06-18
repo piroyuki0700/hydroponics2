8fc11a4f9758b68cef6381a6cb53545d3f45df26
piroyuki0700 <matsuyama0700@gmail.com>
2023-06-18 23:08:35 +0900

    update version.

--- hydroponics.js
+++ hydroponics.js
@@ -20,7 +20,7 @@ const server_uri = 'ws://' + location.hostname + ':10700/'
 //
 $(function(){
   // バージョン
-  $('#version').text('Ver.2023.5.3');
+  $('#version').text('Ver.2023.6.18');
 
   // 最初は非表示にするもの
   $('#setting').hide();	// 設定ページ
