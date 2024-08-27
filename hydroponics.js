9af163f8e2cfc844146156a3545aeb3e15e812f3
piroyuki0700 <matsuyama0700@gmail.com>
2024-08-27 21:11:26 +0900

    add emergency message check

--- hydroponics.js
+++ hydroponics.js
@@ -20,7 +20,7 @@ const server_uri = 'ws://' + location.hostname + ':10700/'
 //
 $(function(){
   // バージョン
-  $('#version').text('Ver.2024.7.17');
+  $('#version').text('Ver.2024.8.27');
 
   // 最初は非表示にするもの
   $('#setting').hide();	// 設定ページ
